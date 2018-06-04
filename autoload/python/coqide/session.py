1bb88d89f7721b06a84c8f749fdf53df64a21177
Thomas Ding <thomasbyding@gmail.com>
2018-06-04 09:39:45 +0800

    add test for session

--- autoload/python/coqide/session.py
+++ autoload/python/coqide/session.py
@@ -1,61 +1,60 @@
 '''Coq session.'''
 
 
-from coqide import vimsupport as vims
 from coqide.coqtopinstance import CoqtopInstance
 from coqide.stm import STM
-from coqide.views import SessionView
 
 
 class Session:
     '''A loaded Coq source file and its coqtop interpreter.'''
 
-    def __init__(self, bufnr, tabpage_view, executor):
+    def __init__(self, view, vim, worker):
         '''Create a new session.'''
         self._coqtop = CoqtopInstance()
         self._coqtop.spawn(['coqtop', '-ideslave', '-main-channel', 'stdfds',
                             '-async-proofs', 'on'])
-        self._view = SessionView(bufnr, tabpage_view)
-        self._executor = executor
-        self._stm = STM(self._coqtop, self._view, lambda _: None)
+        self._view = view
+        self._stm = STM(self._coqtop, self._view, self._on_feedback)
+        self._vim = vim
+        self._worker = worker
 
     def forward_one(self):
         '''Add the next sentence after the tip state to the STM.'''
         start = self._stm.get_tip_stop()
-        sentence = vims.get_sentence_after(start)
-        self._executor.submit(self._stm.add, [sentence])
+        sentence = self._vim.get_sentence_after(start)
+        self._worker.submit(self._stm.add, [sentence])
 
     def backward_one(self):
         '''Backward to the previous state of the tip state.'''
-        self._executor.submit(self._stm.edit_at_prev)
+        self._worker.submit(self._stm.edit_at_prev)
 
     def to_cursor(self):
         '''Forward or backward to the sentence under the cursor.'''
         tip_stop = self._stm.get_tip_stop()
-        cursor = vims.get_cursor()
+        cursor = self._vim.get_cursor()
         if tip_stop < cursor:
             self._forward_between(tip_stop, cursor)
         elif tip_stop > cursor:
-            self._executor.submit(self._stm.edit_at, cursor)
-
-    def draw_view(self):
-        '''Draw the session view in the Vim UI.'''
-        self._view.draw()
+            self._worker.submit(self._stm.edit_at, cursor)
 
     def _forward_between(self, from_mark, to_mark):
         '''Add the sentences between `from_mark` and `to_mark` to the STM.'''
         sentences = []
 
-        sentence = vims.get_sentence_after(from_mark)
-        while sentence.stop <= to_mark:
+        sentence = self._vim.get_sentence_after(from_mark)
+        while sentence and sentence.stop <= to_mark:
             sentences.append(sentence)
-            sentence = vims.get_sentence_after(sentence.stop)
-        self._executor.submit(self._stm.add, sentences)
+            sentence = self._vim.get_sentence_after(sentence.stop)
+        self._worker.submit(self._stm.add, sentences)
 
     def close(self):
         '''Close the session.'''
         self._coqtop.close()
-        self._view.destroy()
         self._coqtop = None
         self._view = None
         self._stm = None
+        self._vim = None
+        self._worker = None
+
+    def _on_feedback(self, feedback):
+        '''A callback to process a feedback.'''
---
+++ autoload/python/test/test_session.py
@@ -0,0 +1,122 @@
+'''The unit test for module `coqide.session.Session`.'''
+
+from unittest import TestCase
+from unittest.mock import patch, Mock
+
+from coqide.session import Session
+from coqide.types import Mark, Sentence
+
+
+# pylint: disable=W0212,C0103,R0201
+class TestSession(TestCase):
+    '''Test for class `coqide.session.Session`.'''
+
+    @staticmethod
+    def _worker_mock():
+        '''Return a mock for worker.
+
+        It calls the function immediately the function is submitted.'''
+        def _submit(func, *args, **kwargs):
+            func(*args, **kwargs)
+
+        worker = Mock()
+        worker.submit.side_effect = _submit
+        return worker
+
+    @patch('coqide.session.CoqtopInstance')
+    @patch('coqide.session.STM')
+    def test_constr(self, STM, CoqtopInstance):
+        '''Test the constructor.'''
+        view = Mock()
+        vim = Mock()
+        worker = Mock()
+        session = Session(view, vim, worker)
+
+        CoqtopInstance.assert_called_once_with()
+        CoqtopInstance.return_value.spawn.assert_called_once_with(
+            ['coqtop', '-ideslave', '-main-channel', 'stdfds',
+             '-async-proofs', 'on'])
+
+        STM.assert_called_once_with(
+            CoqtopInstance.return_value, view, session._on_feedback)
+
+    @patch('coqide.session.CoqtopInstance')
+    @patch('coqide.session.STM')
+    def test_forward_one(self, STM, _):
+        '''Test method `forward_one`.'''
+        stm = STM.return_value
+        view = Mock()
+        vim = Mock()
+        worker = self._worker_mock()
+        session = Session(view, vim, worker)
+
+        sentence = Sentence('Proof.\n', Mark(1, 1), Mark(2, 1))
+        stm.get_tip_stop.side_effect = [Mark(1, 1)]
+        vim.get_sentence_after.side_effect = [sentence]
+
+        session.forward_one()
+
+        stm.add.assert_called_once_with([sentence])
+
+    @patch('coqide.session.CoqtopInstance')
+    @patch('coqide.session.STM')
+    def test_backward_one(self, STM, _):
+        '''Test method `backward_one`.'''
+        stm = STM.return_value
+        view = Mock()
+        vim = Mock()
+        worker = self._worker_mock()
+        session = Session(view, vim, worker)
+
+        session.backward_one()
+        stm.edit_at_prev.assert_called_once_with()
+
+    @patch('coqide.session.CoqtopInstance')
+    @patch('coqide.session.STM')
+    def test_to_cursor_forward(self, STM, _):
+        '''Test method `to_cursor` on going forward.'''
+        stm = STM.return_value
+        view = Mock()
+        vim = Mock()
+        worker = self._worker_mock()
+        session = Session(view, vim, worker)
+
+        sentences = [
+            Sentence('', Mark(2, 3), Mark(3, 5)),
+            Sentence('', Mark(3, 5), Mark(4, 1)),
+            Sentence('', Mark(4, 1), Mark(4, 9)),
+            None
+        ]
+        stm.get_tip_stop.side_effect = [Mark(2, 3)]
+        vim.get_cursor.side_effect = [Mark(4, 9)]
+        vim.get_sentence_after.side_effect = sentences
+
+        session.to_cursor()
+        stm.add.assert_called_once_with(sentences[:-1])
+
+    @patch('coqide.session.CoqtopInstance')
+    @patch('coqide.session.STM')
+    def test_to_cursor_backward(self, STM, _):
+        '''Test method `to_cursor` on going backward.'''
+        stm = STM.return_value
+        view = Mock()
+        vim = Mock()
+        worker = self._worker_mock()
+        session = Session(view, vim, worker)
+
+        stm.get_tip_stop.side_effect = [Mark(4, 9)]
+        vim.get_cursor.side_effect = [Mark(2, 3)]
+
+        session.to_cursor()
+        stm.edit_at.assert_called_once_with(Mark(2, 3))
+
+    @patch('coqide.session.CoqtopInstance')
+    @patch('coqide.session.STM')
+    def test_close(self, _, CoqtopInstance):
+        '''Test method `close`.'''
+        view = Mock()
+        vim = Mock()
+        worker = self._worker_mock()
+        session = Session(view, vim, worker)
+        session.close()
+        CoqtopInstance.return_value.close.assert_called_once_with()
